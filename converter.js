64da99fafb394426696a0374e9ee4ada6deab4af
Adrian Ottiker <adrian@ottiker.com>
2016-11-01 14:09:35 +0100

    add module type

--- converter.js
+++ converter.js
@@ -43,6 +43,12 @@ modules.forEach(dep => {
 
     write(
         '<' + owner + '/' + dep  + '>',
+        'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+        '<http://schema.jillix.net/vocab/Module>'
+    );
+
+    write(
+        '<' + owner + '/' + dep  + '>',
         'http://schema.jillix.net/vocab/gitRepository',
         '"\\"git+https://github.com/' + owner + '/' + dep + '.git\\""'
     );
