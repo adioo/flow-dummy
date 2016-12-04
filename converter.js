0451e7148ad4a69a1b9e01623a6913bd91eacf5b
Adrian Ottiker <adrian@ottiker.com>
2016-12-04 20:47:48 +0100

    convert to new flow config

--- converter.js
+++ converter.js
@@ -1,43 +1,45 @@
 'use strict'
 
 // TODO module descriptors
-const suffixTest = /\.json$/;
 const resolve = require('path').resolve;
 const fs = require('fs');
+const suffixTest = /\.json$/;
 const root = resolve(process.argv[2] || '/home/adioo/Repos') + '/';
 const path = root + 'composition/';
-const instances = {};
+const states = {};
 const files = fs.readdirSync(path);
-const domain = '';//'https://static.jillix.com/'
 const type = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type';
 const dependencies = {};
-const modules = [
-    //"flow-app",
-    "flow-browser",
-    "flow-http",
-    "flow-compress",
-    //"flow-pack",
-    "flow-router",
-    //"flow-service-api",
-    "flow-tools",
-    "flow-static",
-    "flow-streams",
-    //"flow-schema",
-    //"flow-url",
-    //"flow-sendgrid",
-    //"flow-auth",
-    "flow-view",
-    "flow-api",
-    "flow-visualizer",
-    //"flow-remodal",
-    "builder"
-    //"schema"
-];
 
-function getModuleId (name) {
+files.forEach(file => {
 
+    if (!suffixTest.test(file)) {
+        return;
+    }
+
+    try {
+        let state = JSON.parse(fs.readFileSync(path + file));
+        states[state.name] = state;
+    } catch (error) {
+        throw new Error(path + file + '\n' + error);
+    }
+});
+
+for (let state in states) {
+    Convert(states[state]);
+}
+
+/*for (let from in dependencies) {
+    for (let to in dependencies[from]) {
+        write(from, 'http://schema.jillix.net/vocab/dependency', to);
+    }
+}*/
+
+function getMethodIri (module, method) {
+
+    // git+https://github.com/ + module + .git
     let owner = 'jillix';
-    switch (name) {
+    switch (module) {
         case 'flow-view':
         case 'flow-auth':
         case 'flow-streams':
@@ -47,71 +49,14 @@ function getModuleId (name) {
             break;
     }
 
-    return owner + '/' + name;
-}
-
-modules.forEach(dep => {
-
-    let module = getModuleId(dep);
-
-    write(
-        '<' + module + '>',
-        'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-        '<http://schema.jillix.net/vocab/Module>'
-    );
-
-    write(
-        '<' + module  + '>',
-        'http://schema.jillix.net/vocab/gitRepository',
-        '"git+https://github.com/' + module + '.git"'
-    );
-
-    write(
-        '<' + module + '>',
-        'http://schema.org/name',
-        '"' + dep + '"'
-    );
-});
-
-files.forEach(file => {
-
-    if (!suffixTest.test(file)) {
-        return;
-    }
-
-    try {
-        let instance = JSON.parse(fs.readFileSync(path + file));
-        instances[instance.name] = instance;
-    } catch (error) {
-        throw new Error(path + file + '\n' + error);
-    }
-});
-
-for (let instance in instances) {
-    Convert(instances[instance]);
-}
-
-for (let from in dependencies) {
-    for (let to in dependencies[from]) {
-        write(from, 'http://schema.jillix.net/vocab/dependency', to);
-    }
+    return owner + '/' + module + '?' + method;
 }
 
 function write (subject, predicate, object) {
     process.stdout.write(subject + ' <' + predicate + '> ' + object + ' .\n');
 }
 
-function getModuleIri (instance, method) {
-    let module = getModuleId(instances[instance].module);
-
-    if (method) {
-        return '<' + module + '#' + method + '>';
-    } else {
-        return '<' + module + '>';
-    }
-}
-
-function extendInstance (instance, path) {
+function extendInstance (state, path) {
     path = path.split('/');
     let parsed = {
         type: path[0][0]
@@ -119,17 +64,17 @@ function extendInstance (instance, path) {
     path[0] = path[0].substr(1);
 
     if (path.length > 1) {
-        parsed.instance = path[0];
+        parsed.state = path[0];
         parsed.path = path[1];
     } else {
-        parsed.instance = instance;
+        parsed.state = state;
         parsed.path = path[0]
     }
 
     if (parsed.type !== '>') {
-        parsed.path = getModuleIri(parsed.instance, parsed.path);
+        parsed.path = getMethodIri(states[parsed.state].module, parsed.path);
     } else {
-        parsed.path = '<' + domain + parsed.instance + '/' + parsed.path + '>';
+        parsed.path = '_:' + parsed.state + '/' + parsed.path;
     }
 
     return parsed;
@@ -143,173 +88,146 @@ function UID (len) {
     return random;
 }
 
-function Convert (instance) {
-    let instance_name = '<' + domain + instance.name + '>';
+function Convert (state) {
+    if (!state.flow) {
+        return;
+    }
 
-    if (instance.name) {
+    for (let sequence in state.flow) {
+        let sequence_id = '_:' + UID(8);
         write(
-            instance_name,
+            sequence_id,
             'http://schema.org/name',
-            '"' + instance.name + '"'
+            '"' + sequence + '"'
         );
-    }
 
-    write(
-        instance_name,
-        type,
-        '<http://schema.jillix.net/vocab/ModuleInstanceConfig>'
-    );
+        // TODO add sequence to cluster
+        /*write(
+            sequence_id,
+            'http://schema.jillix.net/vocab/cluster',
+            cluster_id
+        );
+        //if (state.name !== path.instance) {
+            //dependencies[state.name] = dependencies[state.name] || {};
+            //dependencies[state.name]['<' + path.instance + '>'] = 1;
+        //}
+        */
 
-    // module
-    if (instance.module) {
+        // roles
+        if (state.roles) {
+            for (let role in state.roles) {
+                write(
+                    sequence_id,
+                    'http://schema.jillix.net/vocab/roles',
+                    '"' + role + '"'
+                );
+            }
+        }
+
+        // type
         write(
-            instance_name,
-            'http://schema.jillix.net/vocab/module',
-            getModuleIri(instance.name)
+            sequence_id,
+            type,
+            '<http://schema.jillix.net/vocab/Sequence>'
         );
-    }
 
-    // roles
-    if (instance.roles) {
-        for (let role in instance.roles) {
+        // end event
+        if (state.flow[sequence].e) {
             write(
-                instance_name,
-                'http://schema.jillix.net/vocab/roles',
-                '"' + role + '"'
+                sequence_id,
+                'http://schema.jillix.net/vocab/onEnd',
+                '_:' + state.flow[sequence].e
             );
         }
-    }
 
-    //flow
-    if (instance.flow) {
-        for (let event in instance.flow) {
-            let event_iri = '<' + domain + instance.name + '/' + event + '>';
+        // error event
+        if (state.flow[sequence].r) {
             write(
-                event_iri,
-                'http://schema.org/name',
-                '"' + event + '"'
+                sequence_id,
+                'http://schema.jillix.net/vocab/onError',
+                '_:' + state.flow[sequence].r
             );
+        }
 
-            write(
-                instance_name,
-                'http://schema.jillix.net/vocab/event',
-                event_iri
-            );
+        // sequence
+        if (state.flow[sequence].d) {
+            let previous;
+            state.flow[sequence].d.forEach((handler, index) => {
 
-            write(
-                event_iri,
-                type,
-                '<http://schema.jillix.net/vocab/FlowEvent>'
-            );
+                let handler_id = '_:' + UID(8);
+                let path;
+                let args;
 
-            // end event
-            if (instance.flow[event].e) {
                 write(
-                    event_iri,
-                    'http://schema.jillix.net/vocab/onEnd',
-                    '<' + domain + instance.flow[event].e + '>'
+                    index === 0 ? sequence_id : previous,
+                    'http://schema.jillix.net/vocab/next',
+                    handler_id
                 );
-            }
+                previous = handler_id;
 
-            // error event
-            if (instance.flow[event].r) {
+                if (typeof handler === 'string') {
+                    path = handler;
+                    args = null;
+                } else {
+                    path = handler[0];
+                    args = handler[1];
+                }
+
+                path = extendInstance(state.name, path); 
+
                 write(
-                    event_iri,
-                    'http://schema.jillix.net/vocab/onError',
-                    '<' + domain + instance.flow[event].r + '>'
+                    handler_id,
+                    'http://schema.jillix.net/vocab/sequence',
+                    sequence_id
                 );
-            }
 
-            // sequence
-            if (instance.flow[event].d) {
-                let previous;
-                instance.flow[event].d.forEach((handler, index) => {
+                write(
+                    handler_id,
+                    type,
+                    '<http://schema.jillix.net/vocab/Handler>'
+                );
 
-                    let handler_id = '_:' + UID(8);
-                    let path;
-                    let args;
-
-                    write(
-                        index === 0 ? event_iri : previous,
-                        'http://schema.jillix.net/vocab/sequence',
-                        handler_id
-                    );
-                    previous = handler_id;
-
-                    if (typeof handler === 'string') {
-                        path = handler;
-                        args = null;
-                    } else {
-                        path = handler[0];
-                        args = handler[1];
-                    }
-
-                    path = extendInstance(instance.name, path);
-                    if (instance.name !== path.instance) {
-                        dependencies[instance_name] = dependencies[instance_name] || {};
-                        dependencies[instance_name]['<' + domain + path.instance + '>'] = 1;
-                    }
-
+                // TODO does this generate redudant args objects?
+                if (args) {
                     write(
                         handler_id,
-                        'http://schema.jillix.net/vocab/event',
-                        event_iri
+                        'http://schema.jillix.net/vocab/args',
+                        '"' + JSON.stringify(args).replace(/"/g, '\\"') + '"'
                     );
+                }
 
-                    write(
-                        handler_id,
-                        type,
-                        '<http://schema.jillix.net/vocab/Sequence>'
-                    );
+                write(
+                    handler_id,
+                    'http://schema.jillix.net/vocab/state',
+                    '"' + state.name + '"'
+                );
 
-                    if (args) {
+                switch (path.type) {
+                    case '.':
+                    case ':':
                         write(
                             handler_id,
-                            'http://schema.jillix.net/vocab/args',
-                            '"' + JSON.stringify(args).replace(/"/g, '\\"') + '"'
+                            'http://schema.jillix.net/vocab/data',
+                            path.path
                         );
-                    }
+                        break;
+                    case '*':
+                        write(
+                            handler_id,
+                            'http://schema.jillix.net/vocab/stream',
+                            path.path
+                        );
+                        break;
 
-                    write(
-                        handler_id,
-                        'http://schema.jillix.net/vocab/instance',
-                        '<' + domain + path.instance + '>'
-                    );
-
-                    switch (path.type) {
-                        case '.':
-                            write(
-                                handler_id,
-                                'http://schema.jillix.net/vocab/onceHandler',
-                                path.path
-                            );
-                            break;
-
-                        case ':':
-                            write(
-                                handler_id,
-                                'http://schema.jillix.net/vocab/dataHandler',
-                                path.path
-                            );
-                            break;
-                        case '*':
-                            write(
-                                handler_id,
-                                'http://schema.jillix.net/vocab/streamHandler',
-                                path.path
-                            );
-                            break;
-
-                        case '>':
-                            write(
-                                handler_id,
-                                'http://schema.jillix.net/vocab/emit',
-                                path.path
-                            );
-                            break;
-                    }
-                });
-            }
+                    case '>':
+                        write(
+                            handler_id,
+                            'http://schema.jillix.net/vocab/emit',
+                            path.path
+                        );
+                        break;
+                }
+            });
         }
     }
 }
