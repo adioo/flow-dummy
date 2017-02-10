b5d01dc540b98ae21e9c61b9480f5d0c82525473
Adrian Ottiker <adrian@ottiker.com>
2017-02-10 16:56:26 +0100

    update converter

--- converter.js
+++ converter.js
@@ -7,7 +7,7 @@ const suffixTest = /\.json$/;
 const root = resolve(process.argv[2] || '.') + '/';
 const env_config = require(root + 'flow.json');
 const path = root + 'network/';
-const states = {};
+const sequences = {};
 const files = fs.readdirSync(path);
 const rdf_syntax = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
 const hashids = {};
@@ -108,15 +108,21 @@ files.forEach(file => {
         return;
     }
 
-    let state;
+    let group;
     try {
-        state = JSON.parse(fs.readFileSync(path + file));
+        group = JSON.parse(fs.readFileSync(path + file));
     } catch (error) {
         throw new Error(path + file + '\n' + error);
     }
 
-    //states[state.name] = state; 
-    states[file] = state; 
+    Object.keys(group).forEach((sequence) => {
+
+        if (sequence[sequence]) {
+            throw new Error('Converter: Duplicate sequence "' + sequence + '".');
+        }
+
+        sequences[sequence] = group[sequence];
+    });
 });
 
 // create env objects
@@ -201,184 +207,186 @@ if (env_config) {
 }
 
 // sequences
-for (let name in states) {
+for (let sequence in sequences) {
+    let seq = sequences[sequence];
+    let sequence_id = '_:' + crypto.createHash('md5').update(sequence).digest('hex');
 
-    // sequence
-    for (let sequence in states[name]) {
-        let seq = states[name][sequence];
-        let sequence_id = '_:' + crypto.createHash('md5').update(sequence).digest('hex');
+    if (!seq[0] || !seq[0].length) {
+        continue;
+    }
+
+    // name
+    write(
+        sequence_id,
+        'http://schema.org/name',
+        getHash(sequence.toUpperCase())
+    ); 
+
+    // type
+    write(
+        sequence_id,
+        rdf_syntax + 'type',
+        '<http://schema.jillix.net/vocab/Sequence>'
+    );
+
+    // roles
+    write(
+        sequence_id,
+        'http://schema.jillix.net/vocab/role',
+        public_role
+    );
+
+    // end event
+    if (seq[2]) {
+        write(
+            sequence_id,
+            'http://schema.jillix.net/vocab/onEnd',
+            '_:' + crypto.createHash('md5').update(seq[2]).digest('hex')
+        );
+    }
+
+    // error event
+    if (seq[1]) {
+        write(
+            sequence_id,
+            'http://schema.jillix.net/vocab/onError',
+            '_:' + crypto.createHash('md5').update(seq[1]).digest('hex')
+        );
+    }
+
+    // handlers
+    let previous;
+
+    // handler
+    seq[0].forEach((handler, index) => {
+
+        let handler_id = UID();
+        let handler_name = typeof handler === 'string' ? 'Emit:' + handler : handler[1] + '/' + handler[2];
 
         // name
         write(
-            sequence_id,
+            handler_id,
             'http://schema.org/name',
-            getHash(sequence.toUpperCase())
-        ); 
-
-        // type
-        write(
-            sequence_id,
-            rdf_syntax + 'type',
-            '<http://schema.jillix.net/vocab/Sequence>'
+            getHash(handler_name) 
         );
 
-        // roles
-        write(
-            sequence_id,
-            'http://schema.jillix.net/vocab/role',
-            public_role
-        );
+        // sequence emit
+        if (typeof handler === 'string') {
 
-        // end event
-        if (seq[2]) {
-            write(
-                sequence_id,
-                'http://schema.jillix.net/vocab/onEnd',
-                '_:' + crypto.createHash('md5').update(seq[2]).digest('hex')
-            );
-        }
-
-        // error event
-        if (seq[1]) {
-            write(
-                sequence_id,
-                'http://schema.jillix.net/vocab/onError',
-                '_:' + crypto.createHash('md5').update(seq[1]).digest('hex')
-            );
-        }
-
-        // handlers
-        let previous;
-
-        // handler
-        seq[0].forEach((handler, index) => {
-
-            let handler_id = UID();
-            let handler_name = typeof handler === 'string' ? 'Emit:' + handler : handler[1] + '/' + handler[2];
-
-            // name
+            // type Emit
             write(
                 handler_id,
-                'http://schema.org/name',
-                getHash(handler_name) 
+                rdf_syntax + 'type',
+                '<http://schema.jillix.net/vocab/Emit>'
             );
 
-            // sequence emit
-            if (typeof handler === 'string') {
+            // sequence
+            write(
+                handler_id,
+                'http://schema.jillix.net/vocab/sequence',
+                '_:' + crypto.createHash('md5').update(handler).digest('hex')
+            );
 
-                // type Emit
-                write(
-                    handler_id,
-                    rdf_syntax + 'type',
-                    '<http://schema.jillix.net/vocab/Emit>'
-                );
+        // data handler
+        } else {
 
-                // sequence
-                write(
-                    handler_id,
-                    'http://schema.jillix.net/vocab/sequence',
-                    '_:' + crypto.createHash('md5').update(handler).digest('hex')
-                );
-
-            // data handler
-            } else {
-
-                // state
+            // state
+            if (typeof handler[3] === 'string') {
                 write(
                     handler_id,
                     'http://schema.jillix.net/vocab/state',
                     getFnState(handler[3])
                 );
-
-                // type data
-                write(
-                    handler_id,
-                    rdf_syntax + 'type',
-                    '<http://schema.jillix.net/vocab/Data>'
-                );
-
-                // function
-                write(
-                    handler_id,
-                    'http://schema.jillix.net/vocab/fn',
-                    '<' + handler[0] + '/' + handler[1] + '?' + handler[2] + '>'
-                );
             }
-            
-            // next
+
+            // type data
             write(
-                index === 0 ? sequence_id : previous,
-                'http://schema.jillix.net/vocab/next',
-                handler_id
+                handler_id,
+                rdf_syntax + 'type',
+                '<http://schema.jillix.net/vocab/Data>'
             );
 
-            previous = handler_id;
-
-            // link back to sequence (owner)
+            // function
             write(
-                sequence_id,
-                'http://schema.jillix.net/vocab/handler',
-                handler_id
+                handler_id,
+                'http://schema.jillix.net/vocab/fn',
+                '<' + handler[0] + '/' + handler[1] + '?' + handler[2] + '>'
             );
+        }
+        
+        // next
+        write(
+            index === 0 ? sequence_id : previous,
+            'http://schema.jillix.net/vocab/next',
+            handler_id
+        );
 
-            // method args
-            if (typeof handler !== 'string' && handler[4]) {
-                let args = JSON.stringify(handler[4]);
+        previous = handler_id;
 
-                // potential emits from args
-                let potential_emits = args.match(/\{FLOW\:([^\}]+)\}/g);
-                let emits = [];
-                if (potential_emits) {
-                    potential_emits.forEach(emit => {
-                        let replace = emit;
-                        emit = emit.slice(6, -1);
-                        emit = '_:' + crypto.createHash('md5').update(emit).digest('hex');
-                        args = args.replace(replace, emit);
-                        emits.push(emit); 
-                    });
-                }
+        // link back to sequence (owner)
+        write(
+            sequence_id,
+            'http://schema.jillix.net/vocab/handler',
+            handler_id
+        );
 
-                const args_uid = UID();
+        // method args
+        if (typeof handler !== 'string' && handler[4]) {
+            let args = JSON.stringify(handler[4]);
 
-                write(
-                    handler_id,
-                    'http://schema.jillix.net/vocab/args',
-                    args_uid
-                );
-
-                // create json edge
-                write(
-                    args_uid,
-                    'http://schema.jillix.net/vocab/json',
-                    getHash(args)
-                );
-
-                // args type
-                write(
-                    args_uid,
-                    rdf_syntax + 'type',
-                    '<http://schema.jillix.net/vocab/Args>'
-                );
-
-                // args name
-                write(
-                    args_uid,
-                    'http://schema.org/name',
-                    getHash("Args:" + handler[2])
-                );
-
-                emits.forEach(emit => {
-                    let key = args_uid + emit;
-                    if (!temp_index[key]) {
-                        temp_index[key] = 1;
-                        write(
-                            args_uid,
-                            'http://schema.jillix.net/vocab/sequence',
-                            emit
-                        );
-                    }
+            // potential emits from args
+            let potential_emits = args.match(/\{FLOW\:([^\}]+)\}/g);
+            let emits = [];
+            if (potential_emits) {
+                potential_emits.forEach(emit => {
+                    let replace = emit;
+                    emit = emit.slice(6, -1);
+                    emit = '_:' + crypto.createHash('md5').update(emit).digest('hex');
+                    args = args.replace(replace, emit);
+                    emits.push(emit); 
                 });
             }
-        });
-    }
+
+            const args_uid = UID();
+
+            write(
+                handler_id,
+                'http://schema.jillix.net/vocab/args',
+                args_uid
+            );
+
+            // create json edge
+            write(
+                args_uid,
+                'http://schema.jillix.net/vocab/json',
+                getHash(args)
+            );
+
+            // args type
+            write(
+                args_uid,
+                rdf_syntax + 'type',
+                '<http://schema.jillix.net/vocab/Args>'
+            );
+
+            // args name
+            write(
+                args_uid,
+                'http://schema.org/name',
+                getHash("Args:" + handler[2])
+            );
+
+            emits.forEach(emit => {
+                let key = args_uid + emit;
+                if (!temp_index[key]) {
+                    temp_index[key] = 1;
+                    write(
+                        args_uid,
+                        'http://schema.jillix.net/vocab/sequence',
+                        emit
+                    );
+                }
+            });
+        }
+    });
 }
